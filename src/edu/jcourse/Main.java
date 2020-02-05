fc90212045749a3fd3be6e1f83d17d905dd56a70
SnowBeLock <karkardro@gmail.com>
2020-02-05 20:27:45 +0200

    Initial commit

---
+++ src/edu/jcourse/Main.java
@@ -0,0 +1,28 @@
+package edu.jcourse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+private static int counter=0;
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Thread> threads=new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            count();
+           Thread t=new Thread(Main::count);
+           threads.add(t);
+           t.start();
+        }
+        for (Thread t :threads
+                ) {t.join();
+        }
+        System.out.println(counter);
+    }
+    private static void count(){
+
+        for (int i = 0; i <1_000_000 ; i++) {
+            counter++;
+        }
+    }
+}
